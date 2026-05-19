960d3e04a46c034223e63563bee939c04c0ff241
anikh174 <hossainanik174@gmail.com>
2026-05-19 12:41:27 +0600

    implement Medical Specialties UI

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,5 @@
 import Banner from "@/components/Banner";
+import BrowseSpecialists from "@/components/BrowseSpecialists";
 import TopSpecialists from "@/components/TopSpecialists";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Banner></Banner>
       <TopSpecialists></TopSpecialists>
+      <BrowseSpecialists></BrowseSpecialists>
     </div>
   );
 }
--- src/components/BrowseSpecialists.jsx
+++ src/components/BrowseSpecialists.jsx
@@ -1,11 +1,45 @@
+import Image from 'next/image';
 import React from 'react';
 
 const BrowseSpecialists = () => {
     return (
-        <div>
-            <div className='space-y-3'>
-                <p className='text-2xl'>Browse by</p>
-                <h3 className='text-4xl'>Medical Specialties</h3>
+        <div  className="max-w-7xl mx-auto my-10">
+            <div className='space-y-3 text-center'>
+                <p className='text-2xl text-[#0a9396]'>Browse by</p>
+                <h3 className='text-4xl text-[#005f73]'>Medical Specialties</h3>
+            </div>
+
+            <div className='mt-8 grid grid-cols-3 lg:grid-cols-6 gap-5 lg:gap-10'>
+                <div className='space-y-3 p-5 border-3 border-[#0a9396] rounded-xl'>
+                    <Image src={'https://st.depositphotos.com/57803962/56899/v/450/depositphotos_568992318-stock-illustration-human-heart-anatomy-icon-flat.jpg'} alt='Cardiology' height={50} width={50} className='mx-auto'></Image>
+
+                    <p className='text-center text-xs md:text-xl font-bold'>Cardiology</p>
+                </div>
+                <div className='space-y-3 p-5 border-3 border-[#0a9396] rounded-xl'>
+                    <Image src={'https://cdn.creativefabrica.com/2021/06/15/Neurology-Icon-Graphics-13449143-1-1-580x385.jpg'} alt='Neurology' height={70} width={70} className='mx-auto'></Image>
+
+                    <p className='text-center text-xs md:text-xl font-bold'>Neurology</p>
+                </div>
+                <div className='space-y-3 p-5 border-3 border-[#0a9396] rounded-xl'>
+                    <Image src={'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTE4jhG5nRFmFoFoBNTw34RLn8HOYOyscyQqw&s'} alt='tooth' height={50} width={50} className='mx-auto'></Image>
+
+                    <p className='text-center text-xs md:text-xl font-bold'>Dentistry</p>
+                </div>
+                <div className='space-y-3 p-5 border-3 border-[#0a9396] rounded-xl'>
+                    <Image src={'https://www.shutterstock.com/image-vector/ophthalmology-icon-ophthalmologist-vector-isolated-600nw-2356898781.jpg'} alt='Ophthalmology' height={50} width={50} className='mx-auto'></Image>
+
+                    <p className='text-center text-xs md:text-xl font-bold'>Ophthalmo...</p>
+                </div>
+                <div className='space-y-3 p-5 border-3 border-[#0a9396] rounded-xl'>
+                    <Image src={'https://thumbs.dreamstime.com/b/ortho-icon-isolated-white-background-your-web-mobile-app-design-ortho-icon-isolated-white-background-133860885.jpg'} alt='Orthopedics' height={50} width={50} className='mx-auto'></Image>
+
+                    <p className='text-center text-xs md:text-xl font-bold'>Orthopedics</p>
+                </div>
+                <div className='space-y-3 p-5 border-3 border-[#0a9396] rounded-xl'>
+                    <Image src={'https://thumbs.dreamstime.com/b/lungs-anatomy-logo-clinic-respiratory-system-transplantation-center-pulmonology-clinic-tuberculosis-pneumonia-asthma-205122802.jpg'} alt='Pulmonology' height={50} width={50} className='mx-auto'></Image>
+
+                    <p className='text-center text-xs md:text-xl font-bold'>Pulmonology</p>
+                </div>
             </div>
         </div>
     );
