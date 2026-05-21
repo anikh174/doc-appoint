fb5f9aa6b18da5773174f76bbfa49ec5fdb10753
anikh174 <hossainanik174@gmail.com>
2026-05-21 22:11:05 +0600

    some changes dashboard's

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -4,12 +4,17 @@ export const metadata = {
   title: "Dashboard - Doc-Appoint",
 };
 
-const DashboardPage =  async() => {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/booking`);
+// added additional
+export const dynamic = "force-dynamic";
+
+const DashboardPage = async () => {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/booking`, {
+    cache: "no-store",
+  });
   const bookings = await res.json();
   return (
     <div className="mt-20">
-        <DashboardContent bookings={bookings}/>
+      <DashboardContent bookings={bookings} />
     </div>
   );
 };
