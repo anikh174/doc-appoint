b1f3698ff12789af9287df9e95cdbaeb0f70bb9a
anikh174 <hossainanik174@gmail.com>
2026-05-21 11:21:18 +0600

    fixed some all-appointment section

--- src/app/all-appointment/page.jsx
+++ src/app/all-appointment/page.jsx
@@ -6,7 +6,7 @@ const AllAppointmentPage = async () => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_CLIENT_URL}/doctors`);
   const doctors = await res.json();
   return (
-    <div className="mt-16 px-5 lg:px-0">
+    <div className="mt-16">
       <div>
         <div className="bg-linear-to-l from-[#0a9396] to-[#005f73] py-10 px-5 md:p-14 lg:p-20 text-center space-y-3 md:space-y-5">
           <h2 className="text-3xl md:text-4xl lg:text-5xl text-white font-bold">
@@ -16,7 +16,7 @@ const AllAppointmentPage = async () => {
             Browse and book from our verified specialist doctors
           </p>
         </div>
-        <div>
+        <div className="px-5 lg:px-0">
           <SearchField className={'lg:w-[600px] w-[100%] mx-auto mt-5'}>
             <Label />
             <SearchField.Group>
@@ -28,8 +28,8 @@ const AllAppointmentPage = async () => {
             <FieldError />
           </SearchField>
         </div>
-        <div className="my-10 space-y-3 max-w-7xl mx-auto">
-          <p>{doctors.length} doctors found</p>
+        <div className="my-10 space-y-3 max-w-7xl mx-auto px-5 lg:px-0">
+          <p className="font-bold text-2xl">{doctors.length} <span className="text-gray-500 text-lg">doctors found</span></p>
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-10">
             {doctors.map((doctor) => (
               <AppointmentsCard
