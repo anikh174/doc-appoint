f535c7aaf1eeb38201cc34e8606e3e36dde94155
anikh174 <hossainanik174@gmail.com>
2026-05-22 08:44:44 +0600

    added No booking available status

--- src/components/BookingInfo.jsx
+++ src/components/BookingInfo.jsx
@@ -1,9 +1,10 @@
-'use client'
+"use client";
 import { Button } from "@heroui/react";
 import { BiEdit } from "react-icons/bi";
 import { DeleteAlert } from "./DeleteAlert";
 import Link from "next/link";
 import EditModal from "./edit-modal/EditModal";
+import { div } from "framer-motion/client";
 
 const BookingInfo = ({ bookings }) => {
   return (
@@ -14,7 +15,15 @@ const BookingInfo = ({ bookings }) => {
           <p>Manage your upcoming and past appointments</p>
         </div>
         <div>
-          <Link href={'/all-appointment'}><Button className={'bg-[#0a9396] text-white font-bold rounded-lg w-[100%]'}>+ New Appointment</Button></Link>
+          <Link href={"/all-appointment"}>
+            <Button
+              className={
+                "bg-[#0a9396] text-white font-bold rounded-lg w-[100%]"
+              }
+            >
+              + New Appointment
+            </Button>
+          </Link>
         </div>
       </div>
 
@@ -32,35 +41,54 @@ const BookingInfo = ({ bookings }) => {
           <p>Total Booking</p>
         </div>
       </div>
-      <div className="space-y-10">
-        {bookings.map((booking) => (
-          <div className="border-l-8 shadow-2xl p-3 rounded-lg border-l-black" key={booking._id}>
-            <div className="flex flex-col md:flex-row md:items-center gap-5">
-              <div className="border-t-2 border-l-5 border-[#0a9396] rounded-lg p-3 w-full">
-                <h2 className="text-xl text-bold text-center mb-5 border-b-2">Doctor: {booking.docName}</h2>
-                <h2>Patient: {booking.name}</h2>
-                <p>Gender: {booking.gender}</p>
-                <p>Date: {booking.departureDate}</p>
-                <p>Time: {booking.appointmentTime}</p>
-                <span>Reason: {booking.reason}</span>
-              </div>
+      {bookings.length === 0 ? (
+        <div className="border-y-4 rounded-xl shadow-2xl border-[#0a9396] text-center p-10">
+          <h1 className="text-2xl font-bold text-gray-800">
+            No Booking Available
+          </h1>
 
-              <div className="border-r-5 border-b-2 border-[#0a9396] rounded-lg p-3 w-full">
-                <div>
-                <h1 className="text-xl text-bold text-center mb-5 border-b-2">Patient's Contact Method</h1>
-                <p>Email: {booking.email}</p>
-                <p>Number: {booking.number}</p>
-              </div>
+          <p className="text-gray-500 mt-2">
+            Please book an appointment first.
+          </p>
+        </div>
+      ) : (
+        <div className="space-y-10">
+          {bookings.map((booking) => (
+            <div
+              className="border-l-8 shadow-2xl p-3 rounded-lg border-l-black"
+              key={booking._id}
+            >
+              <div className="flex flex-col md:flex-row md:items-center gap-5">
+                <div className="border-t-2 border-l-5 border-[#0a9396] rounded-lg p-3 w-full">
+                  <h2 className="text-xl text-bold text-center mb-5 border-b-2">
+                    Doctor: {booking.docName}
+                  </h2>
+                  <h2>Patient: {booking.name}</h2>
+                  <p>Gender: {booking.gender}</p>
+                  <p>Date: {booking.departureDate}</p>
+                  <p>Time: {booking.appointmentTime}</p>
+                  <span>Reason: {booking.reason}</span>
+                </div>
 
-              <div className="mt-2 justify-center gap-5 flex border-l-2 border-r-2 border-[#0a9396] rounded-lg p-3 bg-[#0a9396]/10">
-              <EditModal booking={booking}></EditModal>
-              <DeleteAlert booking={booking}></DeleteAlert>
-            </div>
+                <div className="border-r-5 border-b-2 border-[#0a9396] rounded-lg p-3 w-full">
+                  <div>
+                    <h1 className="text-xl text-bold text-center mb-5 border-b-2">
+                      Patient's Contact Method
+                    </h1>
+                    <p>Email: {booking.email}</p>
+                    <p>Number: {booking.number}</p>
+                  </div>
+
+                  <div className="mt-2 justify-center gap-5 flex border-l-2 border-r-2 border-[#0a9396] rounded-lg p-3 bg-[#0a9396]/10">
+                    <EditModal booking={booking}></EditModal>
+                    <DeleteAlert booking={booking}></DeleteAlert>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
